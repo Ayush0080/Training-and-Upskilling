6b9a916e97ba4986865ed862017180291a42742f
Ayush0080 <payush512@gmail.com>
2025-11-24 19:07:10 +0530

    24-NOV

--- Python/Exercises/print.py
+++ Python/Exercises/print.py
@@ -1,4 +1,4 @@
 int_num = 24601   # int_num is a variable which has been assigned an integer
 print(int_num)    # prints the integer assigned to int_num
 print(True)       # uses print() to display the Boolean value True
-print(1 + 1 + 1)  # prints the result of the expression 1 + 1 + 1
+print(1 + 1 + 1)  # prints the result of the expression 1 + 1 + 1 ###
